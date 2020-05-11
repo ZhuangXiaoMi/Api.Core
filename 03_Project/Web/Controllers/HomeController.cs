9c81f639c2e19f2f3f121a51b564966abd991b38
庄小米 <1009123099@qq.com>
2020-05-11 23:21:18 +0800

    修改 web首页

--- 03_Project/Web/Controllers/HomeController.cs
+++ 03_Project/Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity.SysManage;
+using IService.Sys;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Web.Models;
@@ -12,15 +14,38 @@ namespace Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ISysUserService _sysUserService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ISysUserService sysUserService)
         {
             _logger = logger;
+            _sysUserService = sysUserService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int deptId)
         {
-            return View();
+            ViewBag.Title = $"User Index of {deptId}";
+            var users = await _sysUserService.Query(o => o.DeptId == deptId && o.IsDelete == false, p => p.RealName);
+            return View(users);
+        }
+
+        public IActionResult Add(int deptId)
+        {
+            ViewBag.Title = "Add User";
+            return View(new SysUser
+            {
+                DeptId = deptId
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(SysUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                await _sysUserService.Add(user);
+            }
+            return RedirectToAction(nameof(Index), new { deptId = user.DeptId });//nameof利于Index重命名，匿名类deptId跟Index参数一样
         }
 
         public IActionResult Privacy()
