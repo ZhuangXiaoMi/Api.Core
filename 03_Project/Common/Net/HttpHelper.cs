5910717a33b092ec2c42e961c23074b37472d6ed
庄小米 <1009123099@qq.com>
2020-09-23 23:50:49 +0800

    添加 http帮助类

---
+++ 03_Project/Common/Net/HttpHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Common
+{
+    public class HttpHelper
+    {
+        #region Get
+        public static string HttpGet(string url, Hashtable headht = null, Encoding encodeing = null)
+        {
+            HttpWebRequest request;
+
+            //如果是发送HTTPS请求
+            if (url.StartsWith("https", StringComparison.OrdinalIgnoreCase))
+            {
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                request = WebRequest.Create(url) as HttpWebRequest;
+                request.ProtocolVersion = HttpVersion.Version10;
+            }
+            else
+            {
+                request = WebRequest.Create(url) as HttpWebRequest;
+            }
+            request.Method = "GET";
+            //request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "*/*";
+            request.Timeout = 15000;
+            request.AllowAutoRedirect = false;
+            WebResponse response = null;
+            string responseStr = null;
+            if (headht != null)
+            {
+                foreach (DictionaryEntry item in headht)
+                {
+                    request.Headers.Add(item.Key.ToString(), item.Value.ToString());
+                }
+            }
+
+            try
+            {
+                response = request.GetResponse();
+
+                if (response != null)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream(), (encodeing == null ? Encoding.UTF8 : encodeing));
+                    responseStr = reader.ReadToEnd();
+                    reader.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return responseStr;
+        }
+        #endregion Get
+
+
+
+        #region 公共方法
+        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true; //总是接受  
+        }
+        #endregion 公共方法
+    }
+}
